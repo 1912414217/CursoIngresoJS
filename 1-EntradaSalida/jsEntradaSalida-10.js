4607383793f495dbe04762243ea1322dd0cf9243
1912414217 <52494364+1912414217@users.noreply.github.com>
2019-07-04 12:13:15 -0300

    ejercicios del 5 al 10 hechos

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var importe;
+    var resultado;
+
+    importe = document.getElementById('importe').value;
+
+    importe = (25/100 *importe); //descuento
+
+    document.getElementById('resultado').value = importe;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldo;
+    var resultado;
 
-	
+    sueldo = document.getElementById('sueldo').value;
+
+    resultado = (sueldo * 10/100) + parseInt (sueldo); // sueldo * 1.1 = es igual a lo anterior.. (esto es aumento)
+
+    document.getElementById('resultado').value = resultado;
+
 }
