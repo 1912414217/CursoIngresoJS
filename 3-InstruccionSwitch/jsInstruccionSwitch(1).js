52c1e3c035d86268bebb646e0f94b566675e714c
1912414217 <52494364+1912414217@users.noreply.github.com>
2019-07-10 10:18:25 -0300

    Ejercicio uno de switch terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,28 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
 
+    if (mesDelAño == 'Enero') {
+        alert ("Que comiences bien el año!!!");
+    }
 
+    else {
+        if (mesDelAño == 'Marzo') {
+            alert ("a clases!!!");
+            }
 
+        else {
+                if (mesDelAño == 'Julio'){
+                    alert("se vienen las vacaciones!!!");
+                }
 
+                else {
+                    if (mesDelAño == 'Diciembre'){
+                    alert ("Felices fiestas!!!");
+                        }
+                    }
+            }
+
+        }
+        
 }//FIN DE LA FUNCIÓN
