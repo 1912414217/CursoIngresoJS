c6c933a789770b48086a2d659883b91d47182531
1912414217 <52494364+1912414217@users.noreply.github.com>
2019-07-10 11:31:03 -0300

    ejercicio 3 terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,25 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
+	switch (mesDelAño) {
+        case "Febrero":
+            alert ("Este mes no tiene más de 29 dias");
+        break;
+
+        case "Enero":
+        case "Marzo":
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+        case "Julio":
+        case "Agosto":
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+            alert ("Este mes tiene 30 o más dias");
+        break;
+    }
 
 
 }//FIN DE LA FUNCIÓN
