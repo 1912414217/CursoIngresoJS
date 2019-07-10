958ca1a73ec439f2d2cdd21e4fdfe21f1352721c
1912414217 <52494364+1912414217@users.noreply.github.com>
2019-07-10 12:18:52 -0300

    construcción sin terminar

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,6 +6,16 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    var largo;
+    var ancho;
+    var rectangulo;
+
+    largo = document.getElementById('Largo').value;
+
+    ancho = document.getElementById('Ancho').value;
+
+    rectangulo = parseInt ('largo')
+    
 
 }
 function Circulo () 
