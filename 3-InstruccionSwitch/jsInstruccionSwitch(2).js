e0c23571af634888e65017ea4ab8cf1683a08b2a
1912414217 <52494364+1912414217@users.noreply.github.com>
2019-07-10 11:21:53 -0300

    switch
    Ejercicio 2 terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,26 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño) {
+    case "Septiembre":
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+        alert ("Ya pasamos el frio, ahora el calor");
+    break;
 
+    case "Abril":
+    case "Mayo":
+    case "Junio":
+        alert ("Falta para el invierno");
+    break;
 
-
-
+    case "Julio":
+    case "Agosto":
+        alert ("Abrigate que hace frio");
+    break;
+    }
 }//FIN DE LA FUNCIÓN
