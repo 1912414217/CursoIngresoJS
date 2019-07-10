f03db78e1a5524ef6266d2bac3084476bdb9c50d
1912414217 <52494364+1912414217@users.noreply.github.com>
2019-07-10 11:45:34 -0300

    ejercicio 4 terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,36 @@
 function mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
+{  
+    var mesDelAño = document.getElementById('mes').value;
 	
-	
+	switch (mesDelAño){
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+            alert ("Tiene 31 dias");
+        break;
+
+        case "Febrero":
+            alert ("Tiene 28 dias");
+        break;
+
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert ("Tiene 30 dias");
+        break;
+    }
+}
+
+
+    //default = el "else" de "if", pero del switch.
+
+    /* - switch utiliza la iguadas estricta (===) */
 
 
 
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
