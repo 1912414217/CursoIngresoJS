c5d7cd74b5019e2298e882068b915d0f78d300a5
1912414217 <52494364+1912414217@users.noreply.github.com>
2019-07-10 10:15:56 -0300

    Trabajos 9 y 10
    Corregidos y terminados

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,25 @@
 function mostrar()
-{
-	//Genero el número RANDOM entre 1 y 10 
-	
+{	/* Al presionar el Botón, asignar una nota RANDOM al examen y mostrar:
+"EXCELENTE" para notas igual a 9 o 10,
+"APROBÓ" para notas mayores a 4,
+"Vamos, la proxima se puede" para notas menores a 4 */
 
-}//FIN DE LA FUNCIÓN
+	var nota;
+
+	nota = parseInt( (Math.random() *10) +1);
+
+	if (nota > 8) {
+		alert (+ nota + " EXCELENTE");
+	} 
+	
+	else { 
+		if (nota > 4) {
+				alert (+ nota +" APROBÓ");
+			}
+			else {
+				if (nota < 4) {
+				alert (+ nota +" Vamos, la proxima se puede");
+					}
+				}
+		}	
+}
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -2,5 +2,21 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var numero;
+
+	numero = parseInt( (Math.random() * 10) + 1); 
+
+	alert (+numero);
 
 }//FIN DE LA FUNCIÓN
+
+
+	/* otra forma de hacerlo:
+
+		Math.floor (Math.random()*10) +1;
+
+		(formula para sacar un maximo y un minimo): 
+
+		math.floor (math.random()* (numeroMax / numeroMin)) + numeroMin; (el maximo no esta inlcuido)
+
+	 */
