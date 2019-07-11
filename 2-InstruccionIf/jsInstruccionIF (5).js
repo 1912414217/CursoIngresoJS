810067595927eea21962b09c9c81474329dd2610
alumno <alumno>
2019-07-11 12:20:47 -0300

    no recuerdo

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -3,7 +3,25 @@ function mostrar()
 //tomo la edad  
 // Al ingresar una edad solo debemos informar si la persona NO es adolescente.
 
-    var edad;
+    var precio;
+
+    precio = document.getElementById('edad').value;
+
+    if (precio < 25)
+    {
+        alert ("Barato");
+    }else 
+    {
+        if (precio > 75)
+        {
+        alert ("Caro");
+        }
+    }
+
+    
+
+
+    /* var edad;
 
 
     edad = document.getElementById('edad').value;
@@ -17,6 +35,6 @@ function mostrar()
     if (edad > 17){
         alert ("no es adolescente");
     }
-
+    */
     
 }
