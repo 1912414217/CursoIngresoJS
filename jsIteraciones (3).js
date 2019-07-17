7e73b8d7d8374e0f78ae6bd8124ea8f613c841fb
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-17 11:41:49 -0300

    Delete jsIteraciones (3).js

--- jsIteraciones (3).js
+++
@@ -1,9 +0,0 @@
-function mostrar()
-{
-    var clave = prompt ("ingrese clave: " , "Escriba su clave");
-
-    while (clave != 'utn750'){
-        clave = prompt ("Ingrese su clave nuevamente");
-    }
-
-}//FIN DE LA FUNCIÓN
