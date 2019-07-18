d1e7d1f108dc511be2ce14639a838a1f0eaaeb25
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-18 11:30:03 -0300

    7 y 8 terminados

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,11 +1,38 @@
+//Al presionar el botón pedir números hasta que el usuario quiera, sumar los que son positivos y multiplicar los negativos.
+
 function mostrar()
 {
 
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
+	var numero;
+
+	while (respuesta == 'si')
+	{
+		contador++;
+		numero = prompt ("Ingrese un numero: ");
+
+		numero = parseInt (numero);
+
+		while (isNaN (numero))
+		{
+			numero = prompt ("Ingrese un numero valido: ");
+		}
+
+		if (numero > 0)
+		{
+			positivo = positivo + numero;
+		} else if (numero < 0)
+			{
+				negativo = negativo * numero;
+			}
+
+		respuesta = prompt ("Desea ingresar otro numero? ").toLowerCase();
+	}
+
+
 
 
 document.getElementById('suma').value=positivo;
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,45 @@
+//Al presionar el botón pedir números hasta que el usuario quiera, mostrar el número máximo y el número mínimo ingresado.
+
 function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
 	var respuesta='si';
+	var numero;
+	var max;
+	var min;
 
-	while(respuesta!='no')
+	while (respuesta == 'si')
 	{
-		
-	
+		contador++;
+		numero = prompt ("Ingrese numero: ");
+
+		numero = parseInt (numero);
+
+		while ( isNaN (numero))
+		{
+			numero = prompt ("Ingrese un numero: ");
+		}
+
+
+		if (contador == 1)
+		{
+			max = numero;
+			min = numero;
+		}	
+
+		if (numero >= max)
+		{
+			max = numero;
+		} else if (numero <= min)
+			{
+				min = numero;
+			}
+
+		respuesta = prompt ("Desea ingresar otro numero? ").toLowerCase();
 	}
 
-
-
+	document.getElementById('maximo').value=max;
+	document.getElementById('minimo').value=min;
 
 }//FIN DE LA FUNCIÓN
