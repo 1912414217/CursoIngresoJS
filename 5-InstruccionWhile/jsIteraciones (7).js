a8f0d53b499a296bd56428a60b6ab7b4044bbf12
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-18 10:12:32 -0300

    ejercicio 7 terminado

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,9 +1,29 @@
+//Al presionar el botón pedir números hasta que el USUARIO QUIERA e informar la suma acumulada y el promedio.
+
 function mostrar()
 {
-
-	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+	var contador = 0;
+
+	while (respuesta == 'si')
+	{
+		contador++;
+		numero = prompt ("Ingrese numero: ");
+
+		numero = parseInt (numero);
+
+		while ( isNaN (numero))
+		{
+			numero = prompt ("Ingrese un numero: ");
+		}
+
+		acumulador += numero;
+
+		respuesta = prompt ("Desea ingresar otro numero? ");
+	}
+
 
 
 document.getElementById('suma').value=acumulador;
