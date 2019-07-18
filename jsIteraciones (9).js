806c6945cb99343d34b287e1d84a0e2c50ca6770
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-18 12:21:16 -0300

    9 terminado 10 sin terminar

---
+++ jsIteraciones (9).js
@@ -0,0 +1,48 @@
+//Al presionar el botón pedir números hasta que el usuario quiera, mostrar el número máximo y el número mínimo ingresado.
+
+function mostrar()
+{
+
+	var contador=0;
+	var respuesta='si';
+	var numero;
+	var max;
+	var min;
+
+	while (respuesta == 'si')
+	{
+		contador++;
+		numero = prompt ("Ingrese numero: ");
+
+		numero = parseInt (numero);
+
+		while ( isNaN (numero))
+		{
+			numero = prompt ("Ingrese un numero: ");
+		}
+
+
+		if (contador == 1)
+		{
+			max = numero;
+			min = numero;
+		}else
+			{
+				if (numero >= max)
+				{
+					max = numero;
+				} if (numero <= min)
+					{
+						min = numero;
+					}
+			}
+
+		
+
+		respuesta = prompt ("Desea ingresar otro numero? ").toLowerCase();
+	}
+
+	document.getElementById('maximo').value=max;
+	document.getElementById('minimo').value=min;
+
+}//FIN DE LA FUNCIÓN
