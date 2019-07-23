1b049c84ab7c3678f9207f50b6dcf27c38fd9176
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-23 19:28:09 -0300

    Update jsInstruccionIF (8).js

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -5,15 +5,13 @@ function mostrar()
 /*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA, 
 pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.' */	
 
-    var edad;
+    var edad = parseInt (document.getElementById('edad').value);
     var estadoCivil;
 
-    edad = document.getElementById('edad').value;
-    estadoCivil = document.getElementById('estadoCivil').value;
 
-    // "> = mayor" ; "<" = menor
-    if (edad < 18) {
-        if (estadoCivil != 'soltero')
+    if (estadoCivil == "Soltero" || edad > 17)
+    {
+        alert("Es soltero y no es menor");
     }
 
 }//FIN DE LA FUNCIÓN
