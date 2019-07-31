7a8363006ddb4dea47dcc3718c41765adba42b0d
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 11:01:05 -0300

    listo el 4

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,36 @@
+/*Pedir dos números y mostrar el resultado: 
+Si son iguales los muestro concatenados. 
+Si el primero es mayor, los resto, 
+de lo contrario los sumo. 
+Si la suma es mayor a 10 ,además de mostrar el resultado, muestro el mensaje 
+"la suma es xxx y supero el 10".
+*/
 function mostrar()
 {
+    var primerNumero = parseInt(prompt("Ingrese numero: "));
+    var segundoNumero = parseInt(prompt("Ingrese otro numero: "));
+    var resultado;
+
+    if (primerNumero == segundoNumero)
+    {
+        alert(primerNumero+" y "+segundoNumero+" son iguales");
+    } 
+
+    if (primerNumero > segundoNumero)
+    {
+        resultado = primerNumero - segundoNumero;
+        
+    } else if (primerNumero < segundoNumero)
+        {
+            resultado = primerNumero + segundoNumero;
+        }
+
+        alert ("El resultado es: "+resultado);
+
+
+    if (resultado > 10)
+    {
+        alert("La suma es: "+resultado+" y supero el 10");
+    }
 
 }
