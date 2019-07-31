4ae122aa8360772b6b6813a909c4f11de895e61f
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 08:46:49 -0300

    Ejercicio dos del parcial, terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,9 @@
+//mostrar por alert el mensaje "usted es xxxxx y vive en la localidad de xxxxxxxx".
 function mostrar()
 {
+    var nombre = document.getElementById('elNombre').value;
+    var localidad = document.getElementById('laLocalidad').value;
+
+    alert("Usted es: "+nombre+", Y vive en la localidad de: "+localidad);
   
 }
