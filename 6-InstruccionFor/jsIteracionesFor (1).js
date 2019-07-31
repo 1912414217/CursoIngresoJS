41ed0c65be7be97c2895c872d6b91c28be778bff
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 14:56:05 -0300

    ejercicio uno terminado

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,9 +1,8 @@
 // al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el 1 al 10.
 function mostrar()
-{   
-    var cont;
-
-    for (cont == 1 ; cont < 11 ; cont++){ //se puede poner 1, 2, o ninguno.
-        document.write(+cont+"<br>");
+{
+    for (i = 1 ; i < 11 ; i++)
+    {
+        document.write(+i+"<br>");
     }
 }
