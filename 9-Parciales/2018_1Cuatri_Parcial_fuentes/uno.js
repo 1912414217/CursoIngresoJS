f6ddeddc2630f60f06d7f6b66af34096b61c4271
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 08:40:07 -0300

    Parcial, ejercicio uno: Terminado.

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,12 @@
+// Realizar el algoritmo que pida el ancho y el largo de un rectángulo por prompt y que muestre el perímetro por alert. 
+
 
 function mostrar()
 {
+    var ancho = parseInt(prompt("Ingrese ancho: ", "Ingrese ancho aqui."));
+    var largo = parseInt(prompt("Ingrese largo: ", "Ingrese largo aqui."));
+    var perimetro = (2 * ancho) + (2 * largo);
+
+    alert ("Perimetro: "+perimetro);
 
 }
