4af9a38e6292ee56950ebcc0a76df882f7c85b87
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 12:23:00 -0300

    falta terminar

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,17 @@
-function mostrar()
+/*al presionar el botón pedir un número. 
+mostrar los numeros pares desde el 1 al número ingresado, y mostrar la cantidad de numeros pares encontrados.
+
+*/function mostrar()
 {
+    var pares = 0;
+    var numero = parseInt(prompt("Ingrese un numero"));
 
-
-
-
+    for (i = 1 ; i <= numero ; i++)
+    {
+        if( 1 % 2 == 0 && i != 0)
+        {
+            console.log(i)
+            pares++
+        }        
+    }
 }//FIN DE LA FUNCIÓN
