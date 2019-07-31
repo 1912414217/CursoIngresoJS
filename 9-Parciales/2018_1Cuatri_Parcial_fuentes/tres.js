2066146c8bcaedcbda6f02aa8bf53be98af5bf64
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 08:58:01 -0300

    ejercicio tres del parcial, casi terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,10 @@
+// Pedir por prompt el precio y el porcentaje de descuento, mostrar el precio final con descuento por id.
+
 function mostrar()
 {
+    var precio = parseInt(prompt("ingrese precio: "));
+    var descuento = parseInt(prompt("Ingrese porcentaje de descuento: "));
+    var precioFinal = (descuento /100) * precio;
 
+    document.getElementById('elPrecioFinal').value = precioFinal;
 }
