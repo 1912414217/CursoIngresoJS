43a6a36db95d37dbe253927f8f550b94bf38bc25
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 11:57:22 -0300

    listo el 5

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,14 @@
+//al presionar el botón repetir el pedido de número hasta que ingresemos el 9.
 function mostrar()
 {
+    var numero = parseInt(prompt("Ingrese numero"));
 
-
-
-
+    for (i = 0 ; i < 5000000 ; i++)
+    {
+        numero = prompt("Ingrese numero");
+        if (numero == 9)
+        {
+            break;
+        }
+    }
 }//FIN DE LA FUNCIÓN
