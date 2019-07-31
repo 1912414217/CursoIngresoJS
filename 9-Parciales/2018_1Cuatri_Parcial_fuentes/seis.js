db730fdb1543b9a18b273fd98c8086e9a1c226f7
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-07-31 11:45:26 -0300

    casi el 6
    falta que "if" funcione

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,49 @@
+/* Bienvenidos (SWITCH +IF). 
+Se ingresa una hora. 
+Si está entre las 6 y las 11 mostrar:"es de mañana", 
+si es desde las 12 a las 19: "es de tarde", 
+de lo contrario informar que es de noche. 
+Informar si la hora no es válida. 
+Si es de noche y la hora es menor a 24 mostrar se debe agregar el mensaje : "a dormir". 
+
+Aclaración: hacer un switch y dentro toda la lógica.*/
+
 function mostrar()
 {
+    var hora = document.getElementById('laHora').value;
 
+    switch(hora)
+    {
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '10':
+        case '11':
+            alert("Es de mañana");
+        break;
+
+        case '12':
+        case '13':
+        case '14':
+        case '15':
+        case '16':
+        case '17':
+        case '18':
+        case '19':
+            alert("Es de tarde");
+        break;
+
+       if (hora > 19)
+       {
+           if (hora < 24)
+           {
+                alert("Es de noche");
+                alert("A dormir");
+           }
+       }
+
+        default:
+          alert("la hora no es válida.")
+    }
 }
