41b9815c3bea1ee08851aba59cd51052a84ad6b3
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-08-01 09:49:58 -0300

    terminado el 7

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -7,7 +7,7 @@ function mostrar()
     
     for (i = 1; i <= numero; i++)
     {
-        if(numero % i == 0 && i != 0)
+        if(numero % i == 0 && i != 0) // % = divide el 1er numero por el 2do y te muestra el resto de una division
         {
             console.log(i);
             divisores++;
