17e20d2d22777354a7377d71507d142d7ac6ad52
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-08-01 09:24:12 -0300

    listo el 7

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,17 @@
+/* al presionar el botón pedir un número. mostrar los numeros divisores desde el 1 al número ingresado,
+ y mostrar la cantidad de numeros divisores encontrados.*/ 
 function mostrar()
 {
-
-
-
+    var numero = parseInt(prompt("Ingrese numero: "));
+    var divisores = 0;
+    
+    for (i = 1; i <= numero; i++)
+    {
+        if(numero % i == 0 && i != 0)
+        {
+            console.log(i);
+            divisores++;
+        } 
+    }
 
 }//FIN DE LA FUNCIÓN
