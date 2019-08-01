9bef3e44f383598b247963a42e62b85eb97c1c0f
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-08-01 10:04:19 -0300

    falta terminar

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -2,14 +2,19 @@
 function mostrar()
 {
     var numero = parseInt(prompt("Ingrese numero:"));
-    var numeroPrimo = 0;
+    var numeroPrimo;
 
     for (i = 1; i <= numero; i++)
     {
-        if (numero % i == 0) // % = divide el 1er numero por el 2do y te muestra el resto de una division
+        if (numero / 1 == numero && numero / numero == 1) 
         {
-            console.log(i);
-            numeroPrimo++;
-        }
+            alert("El numero "+numero+" es primo.");
+            break; 
+
+        } else if (numero / 1 != numero && numero / numero != 1)
+            {
+                alert("El numero "+numero+" no es primo.");
+                break; 
+            }
     }
 }//FIN DE LA FUNCIÓN
