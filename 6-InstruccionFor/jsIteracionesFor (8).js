2bf7f8191cc4d112ed5f41fa063b5cf78e1b22f9
Sebastián Cuyé <52494364+sebastiancuye@users.noreply.github.com>
2019-08-01 09:48:17 -0300

    listo el 8

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,15 @@
+//al presionar el botón pedir un número. Informar si el numero es PRIMO o no.
 function mostrar()
 {
+    var numero = parseInt(prompt("Ingrese numero:"));
+    var numeroPrimo = 0;
 
-
-
-
+    for (i = 1; i <= numero; i++)
+    {
+        if (numero % i == 0) // % = divide el 1er numero por el 2do y te muestra el resto de una division
+        {
+            console.log(i);
+            numeroPrimo++;
+        }
+    }
 }//FIN DE LA FUNCIÓN
